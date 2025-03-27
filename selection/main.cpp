7bee1133d27d3a88c30dda79dee22044d1503558
ak4700159 <ak47001@naver.com>
2025-03-27 13:46:19 +0900

    03/27 13:46 selection completed

--- selection/main.cpp
+++ selection/main.cpp
@@ -8,26 +8,28 @@ int main() {
     // 배열 크기 입력 
     int n; cin >> n;
     Selection* selection = new Selection(n);
-    selection->setK();
+    int k = initSetK();
+    cout << "[INFO] kth min search start : k{" << k << "}" << endl;
 
     // MIN 값부터 차례대로 k번째까지 반복해서 찾기
     clock_t startTime = clock();
-    // int kValue = selection->execLinear(0, n*n);
+    int kValue = selection->execLinear(0, n*n -1, k);
     clock_t endTime = clock();
     clock_t elapsedLinear = endTime - startTime;
-    cout << "[INFO] execLinear : " << elapsedLinear << " ms" << endl;
+    cout << "[INFO] execLinear : " << elapsedLinear << " ms" << " | kValue : " << kValue << " | k : " << k << endl;
+    selection->copy();
 
     // 정렬 후 k번째 값 찾기
     startTime = clock();
-    // kValue = selection->execSort(0, n*n);
+    kValue = selection->execSort(0, n*n - 1, k);
     endTime = clock();
     clock_t elapsedSort = endTime - startTime;
-    cout << "[INFO] execSort : " << elapsedSort << " ms" << endl;
+    cout << "[INFO] execSort : " << elapsedSort << " ms" << " | kValue : " << kValue << " | k : " << k << endl;
 
-    if(elapsedLinear > elapsedSort){
+    if(elapsedLinear < elapsedSort){
         cout << "[INFO] linear search is better than sort search" << endl;
     }
-    else if(elapsedLinear < elapsedSort) {
+    else if(elapsedLinear > elapsedSort) {
         cout << "[INFO] sort search is better than linear search" << endl;
     }
     else {
--- selection/selection.h
+++ selection/selection.h
@@ -10,31 +10,31 @@ class Selection{
 private:
     // 원본 배열
     int** arr;
-
+    int** copyArr;
     int n;
-    int k;
 public:
     Selection(int);
     ~Selection();
-    // k번째 작은 값 설정
-    void setK();
     // MIN 값부터 차례대로 k번째까지 반복해서 찾기
-    int execLinear(int, int);
+    int execLinear(int, int, int);
     // 정렬 후 k번째 값 찾기
-    int execSort(int , int);
+    int execSort(int , int, int);
     void exchange(int, int);
     void printN();
+    void copy();
 };
-
 
 Selection::Selection(int n) {
     this->n = n;
     srand(time(NULL));
     arr = new int*[n];
+    copyArr = new int*[n];
    for(int i = 0; i < n; i++) {
         arr[i] = new int[n];
+        copyArr[i] = new int[n];
         for(int j = 0; j < n; j++) {
             arr[i][j] = rand() % (100000) + 1;
+            copyArr[i][j] = arr[i][j];
         }
    }
 }
@@ -42,69 +42,113 @@ Selection::Selection(int n) {
 Selection::~Selection() {
     for(int i = 0; i < n; i++) {
         delete[] arr[i];
+        delete[] copyArr[i];
     }
     delete[] arr;
+    delete[] copyArr;
 }
 
 // k값 지정
-void Selection::setK() {
-    k = rand() % (100000) + 1;
+int initSetK() {
+    int k = rand() % (100000) + 1;
+    return k;
 }
 
-// 정렬 후 k번째 값 찾기
+// 정렬 후 k번째 작은 값 찾기
 /*
     1. 피봇 선정
     2. 피봇을 기준으로 정렬
     3. small group size 획득
     4. 3가지 조건식을 통해 
 */
-int Selection::execSort(int p, int q) {
-    if (p >= q) return;
+int Selection::execSort(int p, int q, int k) {
+    if (q - p + 1 < k) return -1;
+    if (p == q) return arr[p/n][p%n];
 
-    int pivotIdx;
-
-
-    exchange(p, pivotIdx);
-    int pivotValue = arr[p / n][p % n];
-    // i = left index;
-    int i = p + 1;
-    // j = right index;
-    int j = q;
-
-    while (i <= j) {
-        while (i <= q && arr[i / n][i % n] <= pivotValue) i++;
-        while (j > p && arr[j / n][j % n] > pivotValue) j--;
-        if (i < j) {
-            exchange(i, j);
+    int pivotValue = arr[p/n][p%n];
+    // cout << "[INFO] pivotValue : " << pivotValue << endl;
+    int leftDuplicatedIdx = p;
+    int rightDuplicatedIdx = q;
+    int i = p;
+    while(i <= rightDuplicatedIdx) {
+        if (arr[i/n][i%n] < pivotValue) {
+            exchange(i, leftDuplicatedIdx);
+            leftDuplicatedIdx++;
+            i++;
+        } else if (arr[i/n][i%n] > pivotValue) {
+            exchange(i, rightDuplicatedIdx);
+            rightDuplicatedIdx--;
+        } else {
+            i++;
         }
     }
-
-    exchange(p, j); // 피봇 위치 복원
-
-
+    
+    // 왼쪽 그룹(피봇보다 작은 원소들)의 크기
+    int numLess = leftDuplicatedIdx - p;
+    // 피봇과 같은 원소들의 개수 (중간 그룹)
+    int numEqual = rightDuplicatedIdx - leftDuplicatedIdx + 1;
+    
+    if (k <= numLess) {
+        // cout << "[INFO] " << rightDuplicatedIdx + 1 << " ~ " << q << " |  k : " << k - numLess - numEqual << endl;
+        return execSort(p, leftDuplicatedIdx - 1, k);
+    } else if (k > numLess + numEqual) {
+        // cout << "[INFO] " << rightDuplicatedIdx + 1 << " ~ " << q << " |  k : " << k - numLess - numEqual << endl;
+        return execSort(rightDuplicatedIdx + 1, q, k - numLess - numEqual);
+    } else {
+        // cout << "[INFO] pivot search!" << endl;
+        return pivotValue;
+    } 
 }
 
-// MIN 값부터 차례대로 k번째까지 반복해서 찾기
-int Selection::execLinear(int p, int q){
-
-
-    return 0;
+// MIN 값부터 차례대로 k번째까지 반복해서 찾기(K번째 작은 값 찾기)
+int Selection::execLinear(int p, int q, int k){
+    const int MAX_VALUE = 1000000;
+    int minValue; int minIdx;
+    int kCount = 0;
+    while (kCount < k) {
+        minValue = MAX_VALUE; 
+        minIdx = -1;         
+        // 전체 중 가장 작은 값 검색
+        for(int j = p; j <= q; j++) {
+            if(arr[j/n][j%n] < minValue){
+                minValue = arr[j/n][j%n];
+                minIdx = j;
+            }
+        }
+        if(minIdx == -1) break;
+        arr[minIdx/n][minIdx%n] = MAX_VALUE;
+        kCount++;
+        // if(kCount % 5 == 0) cout << "[INFO] kCount : " << kCount << endl; 
+    }
+    return minValue;
 }
  
-
 void Selection::printN() {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << "[" << arr[i][j] << "] ->";
-        }
-        cout << endl;
-   }
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < n; j++) {
+//             cout << "[" << arr[i][j] << "] ->";
+//         }
+//         cout << endl;
+//    }
+//    cout << "---------------------" << endl;
+    for(int i = 0; i < n*n; i++){
+        cout << "[" << arr[i/n][i%n] << "] ->";
+        if((i+1)%n == 0) cout << endl;
+    }
 }
 
 void Selection::exchange(int p, int q) {
     int temp = arr[p/n][p%n];
     arr[p/n][p%n] = arr[q/n][q%n];
     arr[q/n][q%n] = temp;
+    // cout << "[INFO] " << arr[p/n][p%n] <<  " <-> " << arr[q/n][q%n] << endl;
+}
+
+void Selection::copy() {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            arr[i][j] = copyArr[i][j];
+    }
 }
 
 #endif
